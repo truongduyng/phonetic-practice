77a2576d4605b0a7c610b4ccc3497f561b3feb67
Truong Duy <truongduyng@gmail.com>
2017-03-20 11:34:24 +0700

    sanitize input

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -5,16 +5,17 @@ class ChecksController < ApplicationController
 
   def create
     build_check
-    @check.save
+    redirect_to root_path unless @check.save
   end
 
   private
 
   def build_check
-    @check ||= Check.new({ question: question }.merge(params[:check] || {}))
+    @check ||= Check.new(params[:check] || {})
   end
 
   def question
-    'hello'
+    @doc ||= Nokogiri::HTML(open("http://livelifehappy.com/page/#{rand(100)}/"))
+    @doc.css('article div.entry-content p:nth-child(2)')[rand(4)].content
   end
 end
--- app/models/check.rb
+++ app/models/check.rb
@@ -1,33 +1,35 @@
-require 'nokogiri'
 require 'open-uri'
 
 class Check < ActiveType::Object
   attribute :question, :string
   attribute :answer, :string
   attribute :right_answer, default: ''
+  # attribute :us_sound_link, :string, default: nil
 
   validates :question, :answer, presence: true
   before_save :sanitize_input
 
   def result
     question.split(' ').map(&:singularize).each do |q|
-      word = Word.find_by_representation(q) || find_in_cambrige_dict(q)
-      self.right_answer += word.phonetic
+      word = Word.find_by_representation(q) || find_in_cambridge_dict(q)
+      self.right_answer += word.phonetic + ' '
     end
+    self.right_answer.strip!
     self.right_answer == answer
   end
 
   private
 
   def sanitize_input
-    self.question = question.downcase.squish
+    self.question = question.downcase.gsub(/[^a-z_ ]/, '').squish
     self.answer   = answer.downcase.squish
   end
 
-  def find_in_cambrige_dict(query)
+  def find_in_cambridge_dict(query)
     doc  = Nokogiri::HTML(open("http://dictionary.cambridge.org/dictionary/english/#{query}"))
     ipas = doc.css('span[pron-region="US"] span.ipa')
+    # self.us_sound_link = doc.css('span[pron-region="US"] span.sound.us')
     return query if ipas.empty?
-    Word.create!(representation: query, phonetic: ipas.first.content)
+    Word.create!(representation: query, phonetic: ipas.first.content.delete('.'))
   end
 end
